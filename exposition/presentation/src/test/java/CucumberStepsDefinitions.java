5093a3d34c274e6843862edd3a0b68634f6b96fc
Gerome <gerome.parmentier@gmail.com>
2020-10-19 21:28:23 +0200

    add cucumber feature

---
+++ exposition/presentation/src/test/java/CucumberStepsDefinitions.java
@@ -0,0 +1,32 @@
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CucumberStepsDefinitions {
+
+    @Given("a librarian")
+    public void a_librarian() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user adds a book")
+    public void the_user_adds_a_book(DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("i have an error message {string}")
+    public void i_have_an_error_message(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ exposition/presentation/src/test/resources/add_books.feature
@@ -0,0 +1,9 @@
+Feature: add a book
+  a librarian can add a book
+
+Scenario: check title : title size is less than 20
+  Given a librarian
+  When the user adds a book
+  |title|
+  |012345678901234567890|
+  Then i have an error message "MaximumSize [title, 012345678901234567890, 20]"
