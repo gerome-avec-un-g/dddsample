71f377ea056dfc52616cf870381fcc53b7285caa
Gérôme <gerome.parmentier@gmail.com>
2020-11-18 13:03:13 +0100

    post redirect get pattern

--- exposition/template/src/main/java/fr/geromeavecung/dddsample/books/BooksController.java
+++ exposition/template/src/main/java/fr/geromeavecung/dddsample/books/BooksController.java
@@ -7,10 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class BooksController {
@@ -32,26 +35,26 @@ public class BooksController {
     }
 
     @GetMapping("/book-creation")
-    public ModelAndView bookCreationGet() {
+    public ModelAndView bookCreationGet(Model model) {
         ModelAndView modelAndView = new ModelAndView("book-creation");
-        modelAndView.addObject("createBookForm", new CreateBookForm());
+        modelAndView.addAllObjects(model.asMap());
+        if (!modelAndView.getModelMap().containsAttribute("createBookForm")) {
+            modelAndView.addObject("createBookForm", new CreateBookForm());
+        }
         return modelAndView;
     }
 
     @PostMapping("/book-creation")
-    public ModelAndView bookCreationPost(@ModelAttribute CreateBookForm createBookForm) {
+    public RedirectView bookCreationPost(@ModelAttribute CreateBookForm createBookForm, RedirectAttributes redirectAttributes) {
         try {
             booksPresentationService.createBook(createBookForm);
-            ModelAndView modelAndView = new ModelAndView("redirect:book-creation");
-            // TODO forward seems to forward to POST not GET
-            modelAndView.addObject("success", true);
-            return modelAndView;
+            redirectAttributes.addFlashAttribute("success", true);
+            return new RedirectView("/book-creation", true);
         } catch (BusinessException businessException) {
             logger.warn("TODO", businessException);
-            ModelAndView modelAndView = new ModelAndView("book-creation");
-            modelAndView.addObject("createBookForm", createBookForm);
-            modelAndView.addObject("error", businessException);
-            return modelAndView;
+            redirectAttributes.addFlashAttribute("createBookForm", createBookForm);
+            redirectAttributes.addFlashAttribute("error", businessException);
+            return new RedirectView("/book-creation", true);
         }
     }
 
