e12d6a309ff375100343b1a20d9b0dafbed1ceb0
Gérôme <gerome.parmentier@gmail.com>
2020-11-19 22:44:28 +0100

    spring mvc tests

--- exposition/template/src/test/java/fr/geromeavecung/dddsample/books/BooksControllerTest.java
+++ exposition/template/src/test/java/fr/geromeavecung/dddsample/books/BooksControllerTest.java
@@ -1,5 +1,25 @@
 package fr.geromeavecung.dddsample.books;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 class BooksControllerTest {
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new BooksController(null)).build();
+    }
+
+    @Test
+    void books() throws Exception {
+        mockMvc.perform(get("/books")).andExpect(status().isOk());
+    }
 
 }
