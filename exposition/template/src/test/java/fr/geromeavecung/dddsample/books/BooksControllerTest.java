ceecf5b58f668bae4534864d379c44093f6db7c9
Gérôme <gerome.parmentier@gmail.com>
2020-11-20 15:47:17 +0100

    spring mvc tests

--- exposition/template/src/test/java/fr/geromeavecung/dddsample/books/BooksControllerTest.java
+++ exposition/template/src/test/java/fr/geromeavecung/dddsample/books/BooksControllerTest.java
@@ -1,10 +1,14 @@
 package fr.geromeavecung.dddsample.books;
 
+import fr.geromeavecung.businessdomain.books.Author;
+import fr.geromeavecung.businessdomain.books.Book;
+import fr.geromeavecung.businessdomain.books.BookAlreadyExists;
+import fr.geromeavecung.businessdomain.books.Title;
+import fr.geromeavecung.businessdomain.shared.BusinessException;
 import fr.geromeavecung.exposition.presentation.BookSummary;
 import fr.geromeavecung.exposition.presentation.BooksPresentationService;
 import fr.geromeavecung.exposition.presentation.CreateBookForm;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -15,11 +19,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.hamcrest.Matcher;
-
 import static org.hamcrest.CoreMatchers.nullValue;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -62,8 +63,47 @@ class BooksControllerTest {
     }
 
     @Test
-    @Disabled
+    void bookCreationGet_redirect_after_success() throws Exception {
+        mockMvc.perform(get("/book-creation").flashAttr("success", true))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book-creation"))
+                .andExpect(model().attribute("createBookForm", new CreateBookForm()))
+                .andExpect(model().attribute("success", true))
+                .andExpect(model().attribute("error", nullValue()));
+    }
+
+    @Test
+    void bookCreationGet_redirect_after_error() throws Exception {
+        CreateBookForm createBookForm = new CreateBookForm();
+        createBookForm.setAuthor("abc");
+        createBookForm.setTitle("def");
+        Book book = Book.create(Title.create("abc"), Author.create("def"));
+        BusinessException businessException = new BookAlreadyExists(book);
+
+        mockMvc.perform(get("/book-creation")
+                .flashAttr("createBookForm", createBookForm)
+                .flashAttr("error", businessException))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book-creation"))
+                .andExpect(model().attribute("createBookForm", createBookForm))
+                .andExpect(model().attribute("success", nullValue()))
+                .andExpect(model().attribute("error", businessException));
+    }
+
+    @Test
     void bookCreationPost_success() throws Exception {
+        CreateBookForm createBookForm = new CreateBookForm();
+        createBookForm.setAuthor("abc");
+        createBookForm.setTitle("def");
+
+        mockMvc.perform(post("/book-creation").flashAttr("createBookForm", createBookForm))
+                .andExpect(status().is3xxRedirection());
+
+        verify(booksPresentationService).createBook(createBookForm);
+    }
+
+    @Test
+    void bookCreationPost_error() throws Exception {
         CreateBookForm createBookForm = new CreateBookForm();
         createBookForm.setAuthor("abc");
         createBookForm.setTitle("def");
@@ -71,14 +111,11 @@ class BooksControllerTest {
         expectedCreateBookForm.setAuthor("abc");
         expectedCreateBookForm.setTitle("def");
 
-        mockMvc.perform(post("/book-creation").flashAttr("createBookForm", createBookForm))
-                .andExpect(status().isPermanentRedirect())
-                .andExpect(view().name("book-creation"))
-                .andExpect(model().attribute("createBookForm", new CreateBookForm()))
-                .andExpect(model().attribute("success", true))
-                .andExpect(model().attribute("error", nullValue()));
+        Book book = Book.create(Title.create("abc"), Author.create("def"));
+        doThrow(new BookAlreadyExists(book)).when(booksPresentationService).createBook(expectedCreateBookForm);
 
-        verify(booksPresentationService).createBook(expectedCreateBookForm);
+        mockMvc.perform(post("/book-creation").flashAttr("createBookForm", createBookForm))
+                .andExpect(status().is3xxRedirection());
     }
 
 }
