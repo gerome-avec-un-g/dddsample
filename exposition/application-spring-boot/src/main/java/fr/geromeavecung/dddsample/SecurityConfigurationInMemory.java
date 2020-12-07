a089ed16a08372de73811b9452762452915c95f4
Gérôme <gerome.parmentier@gmail.com>
2020-12-07 12:10:03 +0100

    add in memory users

---
+++ exposition/application-spring-boot/src/main/java/fr/geromeavecung/dddsample/SecurityConfigurationInMemory.java
@@ -0,0 +1,36 @@
+package fr.geromeavecung.dddsample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurationInMemory extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("buzz")
+                .password("infinity")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("woody")
+                .password("bullseye")
+                .authorities("ROLE_USER");
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        // fixing exception There is no PasswordEncoder mapped for the id "null"
+        // WARNING : unsafe
+        // adding .password("{noop}password") to auth.inMemoryAuthentication()
+        // in previous method doesn't work
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+
+}
