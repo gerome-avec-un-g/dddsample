4dbbd3981328eb265c72cbb8ab8844d0c8b9a2dd
Gérôme <gerome.parmentier@gmail.com>
2020-12-07 12:32:55 +0100

    add in memory users

--- exposition/application-spring-boot/src/main/java/fr/geromeavecung/dddsample/SecurityConfigurationInMemory.java
+++ exposition/application-spring-boot/src/main/java/fr/geromeavecung/dddsample/SecurityConfigurationInMemory.java
@@ -3,6 +3,7 @@ package fr.geromeavecung.dddsample;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -31,6 +32,10 @@ public class SecurityConfigurationInMemory extends WebSecurityConfigurerAdapter 
         // adding .password("{noop}password") to auth.inMemoryAuthentication()
         // in previous method doesn't work
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override protected void configure(HttpSecurity http) throws Exception {
+
     }
 
 
