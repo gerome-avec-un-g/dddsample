a0f0927905d7c055fbd69faba0d6462f3c07cc5b
gerom <gerome.parmentier@gmail.com>
2021-04-20 18:49:58 +0200

    Cucumber documentation + refactoring

--- exposition/presentation/src/test/java/fr/geromeavecung/exposition/presentation/cucumber/books/BooksConfiguration.java
+++ exposition/presentation/src/test/java/fr/geromeavecung/exposition/presentation/cucumber/books/BooksConfiguration.java
@@ -1,18 +1,10 @@
 package fr.geromeavecung.exposition.presentation.cucumber.books;
 
-import fr.geromeavecung.exposition.presentation.cucumber.books.repositories.BooksInMemory;
-import io.cucumber.spring.CucumberTestContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan("fr.geromeavecung.exposition.presentation.cucumber.books")
 public class BooksConfiguration {
-
-    @Bean
-    @Scope(CucumberTestContext.SCOPE_CUCUMBER_GLUE)
-    BooksInMemory booksInMemory() {
-        return new BooksInMemory();
-    }
 
 }
--- exposition/presentation/src/test/java/fr/geromeavecung/exposition/presentation/cucumber/books/repositories/BooksInMemory.java
+++ exposition/presentation/src/test/java/fr/geromeavecung/exposition/presentation/cucumber/books/repositories/BooksInMemory.java
@@ -2,6 +2,7 @@ package fr.geromeavecung.exposition.presentation.cucumber.books.repositories;
 
 import fr.geromeavecung.businessdomain.books.Book;
 import fr.geromeavecung.businessdomain.books.Books;
+import io.cucumber.spring.CucumberTestContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@Scope("cucumber-glue") //@ScenarioScope  does not seem to work
+@Scope(CucumberTestContext.SCOPE_CUCUMBER_GLUE)
 public class BooksInMemory implements Books {
 
     private final Set<Book> books = new HashSet<>();
